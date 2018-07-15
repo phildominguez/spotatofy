b1b855c955f6a50ca62888ff6f6d744c1e6660bf
Phil Dominguez <phild23@gmail.com>
2018-07-15 12:22:23 -0400

    Adding React routing

--- server.js
+++ server.js
@@ -41,6 +41,15 @@ app.get('/callback', function(req, res) {
   })
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 let port = process.env.PORT || 8888
 console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
 app.listen(port)
